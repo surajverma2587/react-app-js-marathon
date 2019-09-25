653b43cff74bddb6324cd3611ec2698df6c5d786
Samuel Fare <samuel.fare@gmail.com>
2019-09-25 15:02:49 +0100

    removed code for teaching

--- src/App.js
+++ src/App.js
@@ -8,38 +8,10 @@ import Header from './Header';
 class App extends Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-      loading: true,
-      photos: [],
-      favourites: []
-    }
-  }
-  async componentDidMount() {
-    const data = await getPhotoData();
-
-    this.setState({
-      photos: data.data.data.results,
-      loading: false
-    })
   }
 
   render() {
-    if (this.state.loading) {
-      return <h1>Loading...</h1>
-    }
-    return (
-      <div className="App-header">
-        <Header title="Auto Expo" subTitle="For all you car enthusiasts, here is a collection of our top cars of 2019 uploaded by people all over the world. Pick your favourite cars today!!ss" />
-        <div className="container">
-          <div className="row">
-            {
-              renderCards(this.state.photos)
-            }
-          </div>
-        </div>
-      </div>
-    );
+    return <span>Use JS here to fetch data and render photos</span>
   }
 }
 
--- src/AppHelper.js
+++ src/AppHelper.js
@@ -2,20 +2,9 @@ import axios from 'axios';
 import React from 'react';
 import Card from './Card';
 
-const getPhotoData = async () => { 
-    return {} // Get data from the server
-  }
-const renderCards = (photos) => {
-    return []// create map of rendered cards
-}
-
-const renderCard= (photo) =>  {
-    return (
-        <div className="col-sm-4">
-            <Card photo={photo} />
-        </div>
-    )
-}
+const getPhotoData = () => {}
+const renderCards = () => {}
+const renderCard= () =>  {}
 
 export { 
     getPhotoData,
